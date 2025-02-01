740f359a94d7d00be4ee583fc4a78584da3d80e2
Andika Sujanadi <andikasujanadi@gmail.com>
2025-02-01 13:06:49 +0800

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:kamus_banjar_mobile_app/repository/dictionary_repository.dart';
 import 'package:kamus_banjar_mobile_app/service/dictionary_service.dart';
 import 'package:kamus_banjar_mobile_app/view/alphabets_view.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
   const DictionaryService dictionaryService =
       DictionaryService(baseUrl: 'http://kamus-banjar.404notfound.fun');
   const DictionaryRepository dictionaryRepository =
       DictionaryRepository(dictionaryService: dictionaryService);
-
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp(
     dictionaryRepository: dictionaryRepository,
   ));
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
   final DictionaryRepository dictionaryRepository;
 
   const MyApp({super.key, required this.dictionaryRepository});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
